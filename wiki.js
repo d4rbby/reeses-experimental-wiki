9ab904e9a7faa065006987095dda2d43e76572dd
Carver Harrison <pcarverb@hotmail.com>
2017-12-09 15:12:43 -0800

    Update wiki.js

--- wiki.js
+++ wiki.js
@@ -88,7 +88,7 @@ var specialPages = {
         */
         
         jget('FILES', function(data) {
-          var files = data.split('\n');
+          var files = data.split('\n').slice(-1);
           cel.innerHTML = Mustache.render(document.getElementById('tp-results').innerHTML, {
             query: t,
             results: files
@@ -103,7 +103,7 @@ var specialPages = {
       var cet = document.getElementById('title');
       cet.innerText = 'Special/Listing';
       jget('FILES', function(data) {
-        var files = data.split('\n');
+        var files = data.split('\n').slice(-1);
         cel.innerHTML = Mustache.render(document.getElementById('tp-listing').innerHTML, {
           results: files
         }); 
